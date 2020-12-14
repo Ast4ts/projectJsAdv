56ab4e77fee5a26ffaae4ae677ae0592258484fa
Oleg <ast4ts@gmail.com>
2020-12-14 17:40:50 +0200

    FinalV2.0

--- projects/project1/script.js
+++ projects/project1/script.js
@@ -11,7 +11,7 @@ var button = document.querySelector('button');
 var quotes = ['Enjoy present pleasures in such a way as not to injure future ones.' , 'Not going home is already like death.' , 'It is not enough to have a good mind. The main thing is to use it well.' , 'There is an applause superior to that of the multitudes: one\'s own.' , 'The government is unresponsive to the needs of the little man. Under 5\'7", it is impossible to get your congressman on the phone.']
 
 function getRandomQuote() {
-   p.innerText = fileQuotes[Math.floor(Math.random() * quotes.length)];
+   p.innerText = quotes[Math.floor(Math.random() * quotes.length)];
 }
 
 // Get random quote on init
