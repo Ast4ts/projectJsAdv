f17490aa24773d66dd83af947d555e0afd631a40
Oleg <ast4ts@gmail.com>
2020-12-14 17:37:20 +0200

    new employ

--- freeCodeCamp/Reverse a String.js
+++ freeCodeCamp/Reverse a String.js
@@ -1,4 +1,15 @@
 function reverseString(str) {
+
+
+
+
+
+
+    return str;
+}
+
+reverseString("hello");
+function reverseString(str) {
     let out = "";
     let a = str.length - 1;
     for(i = a; i >= 0; i -= 1) {
--- projects/project1/script.js
+++ projects/project1/script.js
@@ -1,18 +1,27 @@
 // 1. navigation - done
 // 2. add event listener - done
 // 3. create data structure with quotes - done
-// 4. get random quote
+// 4. get random quote - done
 // 5. display random quote
 
 // Navigation
 var p = document.getElementById('quote');
 var button = document.querySelector('button');
 
-var quotes = ['1', '2', '3'];
+//var quotes = ['Enjoy present pleasures in such a way as not to injure future ones.' , 'Not going home is already like death.' , 'It is not enough to have a good mind. The main thing is to use it well.' , 'There is an applause superior to that of the multitudes: one\'s own.' , 'The government is unresponsive to the needs of the little man. Under 5\'7", it is impossible to get your congressman on the phone.']
+var helper = {
+
+    /**
+     *  Load file
+     */
+    loadFile: function (fileName) {
+        var file_i = imns.FIO.openNode(file);
+        return imns.FIO.readTextFile(file_i).split('\r\n');
+    }
+var quotes = helper.loadFile(".\\text.txt");
 
 function getRandomQuote() {
-// logic
-    console.log('random here');
+   p.innerText = fileQuotes[Math.floor(Math.random() * quotes.length)];
 }
 
 // Get random quote on init
